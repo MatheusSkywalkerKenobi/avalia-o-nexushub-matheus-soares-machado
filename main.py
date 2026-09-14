5c5e7e770f53ecf1cf5f3291ec4a3077bca714a3
MatheusSkywalkerKenobi <zeusmachado007@gmail.com>
2026-09-14 12:59:55 +0000

    v1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-
+#Etapa 1
+startups = {
+    "nome": "CyberPulse Tech",
+    "segmento": "Segurança da Informação",
+    "ano_adesao": "2026"
+}
+solucoes_ativas =["Firewall IA","Scan de Vulnerabilidades"]
+print("STARTUP:",startups["nome"],"fase",startups["segmento"])
+print("Soluçoes em Andamento:",solucoes_ativas[0],"inicio",startups["ano_adesao"])
+#Etapa 2
+bancada = [
+    [1,0],
+    [0,1]
+]
+print("Status da Bancada N1", bancada[0][0])
+print("Status da Bancada N2", bancada[0][1])
+print("Status da Bancada S1", bancada[0][0])
+print("Status da bancada S2", bancada[0][1])
